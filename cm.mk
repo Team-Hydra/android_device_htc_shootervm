31fe0e2f8ae1bdc342ab07f49218dc260bfc0b63
Dorian Snyder <dastin1015@gmail.com>
2013-03-15 20:53:47 -0700

    shootervm: override TARGET_DEVICE

--- cm.mk
+++ cm.mk
@@ -8,14 +8,14 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/htc/shootervm/shootervm.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter BUILD_FINGERPRINT="virgin_mobile/htc_shooter/shooter:4.0.3/IML74K/372137.2:user/release-keys" PRIVATE_BUILD_DESC="1.13.652.2_R2 CL372137 release-keys"
-
 # Device naming
-PRODUCT_DEVICE := shooter
+PRODUCT_DEVICE := shootervm
 PRODUCT_NAME := cm_shootervm
 PRODUCT_BRAND := virgin_mobile
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := Evo V 4G
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_shooter TARGET_DEVICE=shooter BUILD_FINGERPRINT="virgin_mobile/htc_shooter/shooter:4.0.3/IML74K/372137.2:user/release-keys" PRIVATE_BUILD_DESC="1.13.652.2_R2 CL372137 release-keys"
 
 # Bootanimation
 TARGET_SCREEN_HEIGHT := 960
